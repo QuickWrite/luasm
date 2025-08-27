1f8a3b00a367146977500f6ce8883e1d8d21346a
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2025-08-27 21:13:21 +0200

    Add interpreter example

---
+++ examples/04_interpreter.lua
@@ -0,0 +1,37 @@
+local LuASM = require("luasm")
+
+-- 1. Define the instruction set
+local instructions = {
+    LuASM.instruction("print", { "string" }, {
+        executor = function (instruction, _interpreter)
+            print(instruction.args[1])
+        end
+    })
+}
+
+-- 2. Create a runner (use default settings)
+local asm = LuASM:new(instructions, {
+    syntax = {
+        string = "^\"[%w]*\"",
+        reg    = "^%a[%w]*"
+    }
+})
+
+-- 3. Tokenize a source string
+local src = [[
+print "Hello"
+print "World"
+print "Message"
+]]
+local tokenizer = LuASM.string_tokenizer(src)
+
+-- 4. Parse
+local result = asm:parse(tokenizer)
+
+-- 5. Execution
+local interpreter = LuASM:interpreter(result)
+
+interpreter:next_instruction()
+interpreter:next_instruction()
+interpreter:next_instruction()
+
