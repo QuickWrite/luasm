dca8edbae7b29ea24d97b1ba13d0d89e969ae6b7
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2025-09-01 19:06:28 +0200

    Add examples for comments

---
+++ examples/06_custom_comments.lua
@@ -0,0 +1,34 @@
+local LuASM = require("luasm")
+
+-- 1. Define the instruction set
+local instructions = {
+    LuASM.instruction("mov", {"imm", "reg"}, {}),
+    LuASM.instruction("mov", {"reg", "reg"}, {}),
+    LuASM.instruction("add", {"reg", "reg"}, {}),
+    LuASM.instruction("jmp", {"label"}, {}),
+}
+
+-- 2. Create a runner (use default settings)
+local asm = LuASM:new(instructions, {
+    comment = "=.*$"
+})
+
+-- 3. Tokenize a source string
+local src = [[
+start:  mov 10 r0    = My custom comment
+        add  r0 r1   = This just works
+        jmp  start
+]]
+local tokenizer = LuASM.string_tokenizer(src)
+
+-- 4. Parse
+local result = asm:parse(tokenizer)
+
+print("Lines parsed:", result.parsed_lines)
+for name, info in pairs(result.labels) do
+    print("Label: " .. name .. " -> line: " .. info.location)
+end
+
+for i, instr in ipairs(result.instructions) do
+    print(i, instr.op)   -- currently just the instruction name
+end
