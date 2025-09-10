578ae92a89b79c3afdc488e2678e395355b9613a
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2025-09-10 17:36:08 +0200

    Migrate examples to new system

--- examples/01_inline_parsing.lua
+++ examples/01_inline_parsing.lua
@@ -17,7 +17,7 @@ start:  mov 10 r0
         add  r0 r1
         jmp  start
 ]]
-local tokenizer = LuASM:string_tokenizer(src)
+local tokenizer = asm:string_tokenizer(src)
 
 -- 4. Parse
 local result = asm:parse(tokenizer)
